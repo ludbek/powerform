dd890a136d8c13a0308cf2006ca5558444f05f90
ludbek <sth.srn@gmail.com>
2016-06-06 18:23:12 +0545

    validator and cleaner init

--- index.js
+++ index.js
@@ -1,125 +1,132 @@
-var validate = require('validate.js')
+var validate = require('validate.js');
 var _ = {
   some: require('lodash/some.js'),
   every: require('lodash/every.js'),
   keys: require('lodash/keys.js'),
   omit: require('lodash/omit.js'),
   forEach: require('lodash/forEach.js')
-}
+};
 
 function prop(model, field, defaultValue) {
-  var initialState = defaultValue || ""
-  var state = initialState
+  var initialState = defaultValue || "";
+  var previousState = initialState;
+  var state = model._config[field].decorator
+      ? model._config[field].decorator(initialState, previousState)
+      : initialState;
 
   var aclosure = function (value) {
-    if(arguments.length === 0)  return state
-    state = value
-    }
+    if(arguments.length === 0)  return state;
+
+    previousState = state;
+    state = model._config[field].decorator
+      ? model._config[field].decorator(value, previousState )
+      : value;
+  };
 
   aclosure.isDirty = function() {
-    return initialState !== state
-    }
+    return initialState !== state;
+  };
 
   aclosure.setAndValidate = function (value) {
-    aclosure(value)
-    aclosure.isValid()
-  }
+    aclosure(value);
+    aclosure.isValid();
+  };
 
   aclosure.isValid = function (attach_errors) {
-    var errors
-    var constrains = {}
-    constrains[field] = _.omit(model._config[field], ['default'])
-    var values = {}
-    values[field] = aclosure()
+    var errors;
+    var constrains = {};
+    constrains[field] = _.omit(model._config[field], ['default', 'decorator', 'cleaner']);
+    var values = {};
+    values[field] = aclosure();
 
     // for equality
     if (model._config[field].equality) {
-      var equalAgainst = model._config[field].equality
-      values[equalAgainst] = model[equalAgainst]()
+      var equalAgainst = model._config[field].equality;
+      values[equalAgainst] = model[equalAgainst]();
       }
 
-    errors = validate(values, constrains)
+    errors = validate(values, constrains);
     if(attach_errors !== false) {
-      aclosure.errors(errors? errors[field]: undefined)
+      aclosure.errors(errors? errors[field]: undefined);
     }
 
-    return errors === undefined
-  }
+    return errors === undefined;
+  };
 
   aclosure.reset = function () {
-    aclosure(initialState)
-    aclosure.errors(undefined)
-  }
+    aclosure(initialState);
+    aclosure.errors(undefined);
+  };
 
   aclosure.errors = function () {
-    var state
+    var state;
     return function (errors) {
-      if (arguments.length === 0) return state
-      state = errors
-    }
-  }()
+      if (arguments.length === 0) return state;
+      state = errors;
+    };
+  }();
 
-  return aclosure
-  }
+  return aclosure;
+}
 
 module.exports =  function (config) {
   var formModel = {
     _config: config,
     isValid: function (attach_errors) {
-      var self = this
-      var truthPool = []
+      var self = this;
+      var truthPool = [];
       _.forEach(config, function (avalue, akey) {
-        truthPool.push(self[akey].isValid(attach_errors))
-        })
+        truthPool.push(self[akey].isValid(attach_errors));
+      });
 
-      return _.every(truthPool, function (value) { return value === true})
-      },
+      return _.every(truthPool, function (value) { return value === true;});
+    },
 
     isDirty: function () {
-      var self = this
+      var self = this;
       return _.some(_.keys(this._config), function (akey) {
-        return self[akey].isDirty()
-        });
-      },
+        return self[akey].isDirty();
+      });
+    },
 
     data: function () {
-      var dict = {}
-      var self = this
+      var dict = {};
+      var self = this;
       _.forEach(this._config, function (avalue, akey) {
-        dict[akey] = self[akey]()
-        })
+        dict[akey] = avalue.cleaner? avalue.cleaner(self[akey]()): self[akey]();
+      });
 
       return dict;
-      },
+    },
 
     errors: function (supplied_errors) {
-      var dict = {}
-      var self = this
+      var dict = {};
+      var self = this;
 
       if (arguments.length === 0) {
         _.forEach(config, function (avalue, akey) {
-          dict[akey] = self[akey].errors()
-          })
-        return dict
-        }
+          dict[akey] = self[akey].errors();
+        });
+        return dict;
+      }
       else {
         _.forEach(config, function (avalue, akey) {
-          self[akey].errors(supplied_errors[akey]? supplied_errors[akey]: undefined)
-          })
-        }
-      },
+          self[akey].errors(supplied_errors[akey]? supplied_errors[akey]: undefined);
+        });
+      };
+    },
 
     reset: function () {
       _.forEach(config, function (avalue, akey) {
-        formModel[akey].reset()
-        formModel[akey].errors(undefined)
-        })
-      }
-    };
+        formModel[akey].reset();
+        formModel[akey].errors(undefined);
+      });
+    }
+  };
 
   _.forEach(config, function (avalue, akey) {
-    formModel[akey] = prop(formModel, akey, avalue.default)
-    })
+    formModel[akey] = prop(formModel, akey, avalue.default);
+  });
 
-  return formModel
-  }
+  return formModel;
+};
--- spec/index-spec.js
+++ spec/index-spec.js
@@ -1,295 +1,321 @@
-var Form = require("../index.js")
+var Form = require("../index.js");
 
 describe("Form", function () {
-  var formModel
+  var formModel;
   var config = {
     username: {default: 'batman', presence: true},
-    password: {}}
+    password: {}};
   beforeEach(function () {
-    formModel = new Form(config)
-    })
+    formModel = new Form(config);
+  });
 
   it("constructs an object", function () {
-    expect(formModel).toBeDefined()
-    })
+    expect(formModel).toBeDefined();
+  });
 
   it("turns keys on config dict to the attributes of the returned object", function () {
-    expect(formModel.username).toBeDefined()
-    expect(formModel.password).toBeDefined()
-    })
+    expect(formModel.username).toBeDefined();
+    expect(formModel.password).toBeDefined();
+  });
 
   it("attaches original config to ._config attribute of the object", function () {
-    expect(formModel._config).toEqual(config)
-    })
+    expect(formModel._config).toEqual(config);
+  });
 
   it("sets default value to each attribute", function () {
-    expect(formModel.username()).toEqual('batman')
-    })
+    expect(formModel.username()).toEqual('batman');
+  });
 
   it("sets .isValid() method", function () {
-    expect(formModel.isValid).toBeDefined()
-    })
+    expect(formModel.isValid).toBeDefined();
+  });
 
   it("sets .isDirty() method", function () {
-    expect(formModel.isDirty).toBeDefined()
-    })
+    expect(formModel.isDirty).toBeDefined();
+  });
 
   describe(".aProp", function () {
-    var aform
+    var aform;
     beforeEach(function () {
       aform = new Form({
         username: {presence: true},
-        password: {}})})
+        password: {}});
+    });
 
-    it("sets '' default value", function () {
-      expect(aform.username()).toEqual('')})
+    it("sets '' as default value", function () {
+      expect(aform.username()).toEqual('');
+    });
 
     it("sets default value", function () {
-      var aform = new Form({username: {default: "batman"}})
-      expect(aform.username()).toEqual("batman")})
+      var aform = new Form({username: {default: "batman"}});
+      expect(aform.username()).toEqual("batman");
+    });
 
     it("is a getter and setter", function () {
       aform.username('superman');
-      expect(aform.username()).toEqual('superman')})
+      expect(aform.username()).toEqual('superman');
+    });
+
+    it("calls decorator with new and old values", function () {
+      var newValue, oldValue;
+      var aform = new Form({username: {default: "batman",
+                                       decorator: function (newState, oldState) {
+                                         newValue = newState;
+                                         oldValue = oldState;
+                                         return newState;
+                                       }}});
+      aform.username("superman");
+      expect(newValue).toEqual("superman");
+      expect(oldValue).toEqual("batman");
+    });
 
     describe(".isDirty()", function () {
-      var aform = new Form({username: {default: 'ausername', presence: true}})
+      var aform = new Form({username: {default: 'ausername', presence: true}});
 
       it("returns false if the value has not been altered", function () {
-        expect(aform.username.isDirty()).toEqual(false)
-        });
+        expect(aform.username.isDirty()).toEqual(false);
+      });
 
       it("returns true if the value has been altered", function () {
-        aform.username('busername')
-        expect(aform.username.isDirty()).toEqual(true)
-        })
-      })
+        aform.username('busername');
+        expect(aform.username.isDirty()).toEqual(true);
+      });
+    });
 
     describe(".isValid()", function () {
       it("assigns errors to .errors attribute of itself too", function () {
-        formModel.username("")
-        formModel.username.isValid()
-        expect(formModel.username.errors).toBeDefined()
-        })
+        formModel.username("");
+        formModel.username.isValid();
+        expect(formModel.username.errors).toBeDefined();
+      });
 
       it("empties the error field if valid value is supplied", function() {
-        formModel.username('spiderman')
-        formModel.username.isValid()
-        expect(formModel.errors.username).not.toBeDefined()
-        })
+        formModel.username('spiderman');
+        formModel.username.isValid();
+        expect(formModel.errors.username).not.toBeDefined();
+      });
 
       it("validates against dependent field if equality is set", function () {
         var form = new Form({
           password: {presence: true},
-          confirmPassword: {equality: "password"}})
+          confirmPassword: {equality: "password"}});
 
-        form.password("flash")
-        form.confirmPassword("quicksilver")
-        form.confirmPassword.isValid()
-        expect(form.confirmPassword.errors()).toBeDefined()
+        form.password("flash");
+        form.confirmPassword("quicksilver");
+        form.confirmPassword.isValid();
+        expect(form.confirmPassword.errors()).toBeDefined();
 
-        form.confirmPassword("flash")
-        form.confirmPassword.isValid()
-        expect(form.errors.confirmPassword).not.toBeDefined()
-      })
+        form.confirmPassword("flash");
+        form.confirmPassword.isValid();
+        expect(form.errors.confirmPassword).not.toBeDefined();
+      });
 
       it("returns true if the property is valid", function () {
-        formModel.username("batman")
-        expect(formModel.username.isValid()).toEqual(true)
-      })
+        formModel.username("batman");
+        expect(formModel.username.isValid()).toEqual(true);
+      });
 
       it("returns false if the proerty is invalid", function () {
-        formModel.username("")
-        expect(formModel.username.isValid()).toEqual(false)
-      })
+        formModel.username("");
+        expect(formModel.username.isValid()).toEqual(false);
+      });
 
       it("does not set the errors if 'false' is passed", function () {
-        formModel.username("batman")
-        formModel.username.isValid()
-        formModel.username("")
-        formModel.username.isValid(false)
-        expect(formModel.username.errors()).not.toBeDefined()
-      })
-     })
+        formModel.username("batman");
+        formModel.username.isValid();
+        formModel.username("");
+        formModel.username.isValid(false);
+        expect(formModel.username.errors()).not.toBeDefined();
+      });
+    });
 
     describe(".setAndValidate()", function () {
-      var aform
+      var aform;
       beforeEach(function () {
-        aform = new Form({username: {presence: true}})
-      })
+        aform = new Form({username: {presence: true}});
+      });
 
       it("sets the value", function () {
-        aform.username.setAndValidate("ausername")
-        expect(aform.username()).toEqual("ausername")
-      })
+        aform.username.setAndValidate("ausername");
+        expect(aform.username()).toEqual("ausername");
+      });
 
       it("validates the value", function () {
-        aform.username.setAndValidate("")
-        expect(aform.username.errors).toBeDefined()
-      })
-    })
+        aform.username.setAndValidate("");
+        expect(aform.username.errors).toBeDefined();
+      });
+    });
 
     describe(".reset()", function () {
-      var aform
+      var aform;
       beforeEach(function () {
-        aform = new Form({username: {presence: true, default: "baba"}})
-      })
+        aform = new Form({username: {presence: true, default: "baba"}});
+      });
 
       it("resets the value", function () {
-        aform.username("rara")
-        aform.username.reset()
-        expect(aform.username()).toEqual("baba")
-      })
+        aform.username("rara");
+        aform.username.reset();
+        expect(aform.username()).toEqual("baba");
+      });
 
       it("empties its errors", function () {
-        aform.username("")
-        aform.username.isValid()
-        expect(aform.username.errors()).toBeDefined()
-        aform.username.reset()
-        expect(aform.username.errors()).not.toBeDefined()
-      })
-    })
+        aform.username("");
+        aform.username.isValid();
+        expect(aform.username.errors()).toBeDefined();
+        aform.username.reset();
+        expect(aform.username.errors()).not.toBeDefined();
+      });
+    });
 
     describe(".errors()", function () {
-      var aform
+      var aform;
       beforeEach(function () {
-        aform = new Form({username: {presence: true}})
-      })
+        aform = new Form({username: {presence: true}});
+      });
 
       it("gets/sets the errors", function () {
-        aform.username.errors("a error")
-        expect(aform.username.errors()).toEqual("a error")
-      })
+        aform.username.errors("a error");
+        expect(aform.username.errors()).toEqual("a error");
+      });
 
       it("can set 'undefined' as an error", function () {
-        aform.username.errors(undefined)
-        expect(aform.username.errors()).toEqual(undefined)
-      })
-    })
-  })
+        aform.username.errors(undefined);
+        expect(aform.username.errors()).toEqual(undefined);
+      });
+    });
+  });
 
   describe(".isValid()", function () {
-    var aform
+    var aform;
     beforeEach(function () {
       aform = new Form({
         username: {presence: true},
-        password: {presence: true}})
-    })
+        password: {presence: true}});
+    });
 
     it("returns true if form is valid", function () {
-      aform.username("ausername")
-      aform.password("apassword")
-      expect(aform.isValid()).toEqual(true)
-      })
+      aform.username("ausername");
+      aform.password("apassword");
+      expect(aform.isValid()).toEqual(true);
+    });
 
     it("returns false if form is invalid", function () {
-      aform.username("")
-      aform.password("apassword")
-      expect(aform.isValid()).toEqual(false)
-      })
+      aform.username("");
+      aform.password("apassword");
+      expect(aform.isValid()).toEqual(false);
+    });
 
     it("sets errors if nothing is passed", function () {
-      aform.username("")
-      aform.password("apassword")
-      aform.isValid()
-      expect(aform.errors()["username"]).toBeDefined()
-      expect(aform.errors()["password"]).not.toBeDefined()
-    })
+      aform.username("");
+      aform.password("apassword");
+      aform.isValid();
+      expect(aform.errors()["username"]).toBeDefined();
+      expect(aform.errors()["password"]).not.toBeDefined();
+    });
 
     it("does not change errors if 'false' is passed", function () {
-      aform.username("")
-      aform.password("")
-      aform.isValid(false)
-      expect(aform.errors()["username"]).not.toBeDefined()
-      expect(aform.errors()["password"]).not.toBeDefined()
-    })
+      aform.username("");
+      aform.password("");
+      aform.isValid(false);
+      expect(aform.errors()["username"]).not.toBeDefined();
+      expect(aform.errors()["password"]).not.toBeDefined();
+    });
 
     it("sets each property's errors to undefined if form validates", function () {
-      aform.username("ausername")
-      aform.username("apassword")
-      aform.isValid()
-      expect(aform.errors()["username"]).not.toBeDefined()
-      expect(aform.errors()["apassword"]).not.toBeDefined()
-    })
+      aform.username("ausername");
+      aform.username("apassword");
+      aform.isValid();
+      expect(aform.errors()["username"]).not.toBeDefined();
+      expect(aform.errors()["apassword"]).not.toBeDefined();
+    });
 
     it("it sets errors on individual properties", function () {
-      aform.username("")
-      aform.password("hello")
-      aform.isValid()
-      expect(aform.username.errors()).toBeDefined()
-      expect(aform.password.errors()).not.toBeDefined()
-    })
-  })
+      aform.username("");
+      aform.password("hello");
+      aform.isValid();
+      expect(aform.username.errors()).toBeDefined();
+      expect(aform.password.errors()).not.toBeDefined();
+    });
+  });
 
   describe(".isDirty()", function () {
-    var aform = new Form({username: {presence: true, default: 'ausername'}})
+    var aform = new Form({username: {presence: true, default: 'ausername'}});
 
     it("returns false if form has not been altered", function () {
-      expect(aform.isDirty()).toEqual(false)
-      })
+      expect(aform.isDirty()).toEqual(false);
+    });
 
     it("returns true if form has been altered", function () {
-      aform.username('busername')
-      expect(aform.isDirty()).toEqual(true)
-      })
-    })
+      aform.username('busername');
+      expect(aform.isDirty()).toEqual(true);
+    });
+  });
 
   describe(".data()", function () {
-    var aform
+    var aform;
     beforeAll(function () {
-      aform = new Form(
-        {username: {default: 'ausername'},
-        password: {default: 'apassword'}})
-      })
+      aform = new Form({username: {default: 'ausername'},
+                        password: {default: 'apassword'}});
+    });
 
     it("returns the dict with key:value pair for each form field", function () {
-      expect(aform.data()).toEqual({username: 'ausername', password: 'apassword'})
-      })
-    })
+      expect(aform.data()).toEqual({username: 'ausername', password: 'apassword'});
+    });
+
+    it("cleans the data if cleaner if present", function () {
+      var cleaner = function (data) {
+        return data.replace("a", "");
+      };
+
+      var aform = new Form({username: {default: 'ausername', cleaner: cleaner},
+                            password: {default: 'apassword', cleaner: cleaner}});
+      expect(aform.data()).toEqual({username: 'username', password: 'password'});
+    });
+  });
 
   describe(".errors()", function () {
-    var aform
+    var aform;
     beforeAll(function () {
       aform = new Form(
         {username: {default: 'ausername'},
-        password: {default: 'apassword'}})
-      })
+         password: {default: 'apassword'}});
+    });
 
     it("sets errors on each property", function () {
-      aform.errors({username: "a error"})
-      expect(aform.username.errors()).toEqual("a error")
-      expect(aform.password.errors()).not.toBeDefined()
-    })
+      aform.errors({username: "a error"});
+      expect(aform.username.errors()).toEqual("a error");
+      expect(aform.password.errors()).not.toBeDefined();
+    });
 
     it("returns the error of each property", function () {
-      var errors = {username: "a error", password: "a error"}
-      aform.errors(errors)
-      expect(aform.errors()).toEqual(errors)
-    })
-  })
+      var errors = {username: "a error", password: "a error"};
+      aform.errors(errors);
+      expect(aform.errors()).toEqual(errors);
+    });
+  });
 
   describe(".reset()", function () {
-    var aform
+    var aform;
     beforeEach(function () {
       aform = new Form({
         username: {presence: true, default: "ausername"},
-        password: {presence: true, default: "apassword"}})
-    })
+        password: {presence: true, default: "apassword"}});
+    });
 
     it("resets value of each property", function () {
-      aform.username("busername")
-      aform.password("bpassword")
-      aform.reset()
-      expect(aform.username()).toEqual("ausername")
-      expect(aform.password()).toEqual("apassword")
-    })
+      aform.username("busername");
+      aform.password("bpassword");
+      aform.reset();
+      expect(aform.username()).toEqual("ausername");
+      expect(aform.password()).toEqual("apassword");
+    });
 
     it("empties errors on each property", function () {
-      aform.username.errors("a error")
-      aform.password.errors("b password")
-      aform.reset()
-      expect(aform.username.errors()).not.toBeDefined()
-      expect(aform.password.errors()).not.toBeDefined()
-    })
-  })
-})
+      aform.username.errors("a error");
+      aform.password.errors("b password");
+      aform.reset();
+      expect(aform.username.errors()).not.toBeDefined();
+      expect(aform.password.errors()).not.toBeDefined();
+    });
+  });
+});
