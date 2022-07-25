dc331c90e358acc13e3f5d97cced0fd4d54f3bd9
suren <sth.srn@gmail.com>
2022-07-25 11:31:10 +1000

    new Field and Form init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 const STOP_VALIDATION_ERROR_NAME = "StopValidationError";
 
+class ValidationError extends Error {}
+
 class StopValidationError extends Error {
   constructor() {
     super();
@@ -11,54 +13,148 @@ const isEqual = (val1: unknown, val2: unknown): boolean => {
   return JSON.stringify(val1) === JSON.stringify(val2);
 };
 
-export class Field<T, F> {
-  private error: string
-  private previousValue: T
-  private currentValue: T
-  constructor(private validator: Validator<T, F>, private initialValue: T, private fieldName: string, private parent: Form<F>) {
-    this.validator = validator;
-    this.error = "";
-    this.currentValue = this.previousValue = this.initialValue;
-    this.setData(this.initialValue, true);
-    this.makePristine();
+type Optional<T> = T | undefined;
+function optional<T>(decoder: Decoder<T>) {
+  return (val: unknown): [Optional<T>, Error] => {
+    if (val === "") return [undefined, ""];
+    return decoder(val);
+  };
+}
+
+type Error = string;
+type Decoder<T> = (val: unknown) => [T, Error];
+type ChangeHandler<T> = (val: T) => void;
+type InputHandler = (val: string, preVal: string) => string;
+type ErrorHandler = (error: string) => void;
+export class Field<T> {
+  changeHandler?: ChangeHandler<T>;
+  inputHandler?: InputHandler;
+  errorHandler?: ErrorHandler;
+  fieldName: string = "";
+  form?: any;
+  private error: string = "";
+
+  // html input field value is always string no matter
+  // what its type is, type is only for UI
+  private initialValue: string = "";
+  private previousValue: string = "";
+  private currentValue: string = "";
+
+  private validators: Validator<NoUndefined<T>>[];
+
+  constructor(
+    private decoder: Decoder<T>,
+    ...validators: Validator<NoUndefined<T>>[]
+  ) {
+    this.validators = validators;
+  }
+
+  stringify(val: any) {
+    return typeof val === "string" ? val : JSON.stringify(val);
+  }
+
+  optional() {
+    const optionalDecoder = optional(this.decoder);
+    return new Field<Optional<T>>(
+      optionalDecoder,
+      ...(this.validators as Validator<NoUndefined<T>>[])
+    );
+  }
+
+  initValue(val: any) {
+    const strVal = this.stringify(val);
+    this.initialValue =
+      this.previousValue =
+      this.currentValue =
+        this.inputHandler
+          ? this.inputHandler(strVal, this.previousValue)
+          : strVal;
+  }
+
+  onInput(i: InputHandler) {
+    this.inputHandler = i;
+    return this;
+  }
+
+  onChange(c: ChangeHandler<T>) {
+    this.changeHandler = c;
+    return this;
   }
 
   triggerOnError() {
-    if (this.parent) this.parent.triggerOnError();
+    const callback = this.errorHandler;
+    callback && callback(this.getError());
+
+    if (this.form) this.form.triggerOnError();
   }
 
   triggerOnChange() {
-    this.parent && this.parent.triggerOnChange();
+    const callback = this.changeHandler;
+    const [val, err] = this.decoder(this.currentValue);
+    if (err !== "") {
+      return;
+    }
+    callback && callback(val);
+    this.form && this.form.triggerOnChange();
   }
 
-  setData(value: T, skipTrigger?: boolean) {
-    if (isEqual(this.currentValue, value)) return;
+  setValue(val: any, skipTrigger?: boolean) {
+    const strVal = this.stringify(val);
+    if (isEqual(this.currentValue, strVal)) return;
     this.previousValue = this.currentValue;
+    this.currentValue = this.inputHandler
+      ? this.inputHandler(strVal, this.previousValue)
+      : strVal;
 
     if (skipTrigger) return;
     this.triggerOnChange();
   }
 
-  getData() {
+  getRaw(): string {
     return this.currentValue;
   }
 
-  validate() {
-    const [_, err] = this.validator(this.currentValue, {
-      prevValue: this.previousValue,
-      fieldName: this.fieldName,
-      all: this.parent.getData()
-    })
+  getValue(): T {
+    const [val, err] = this.decoder(this.currentValue);
+    if (err !== "")
+      throw new ValidationError(`Invalid value at ${this.fieldName}`);
+    return val;
+  }
+
+  _validate(): string | undefined {
+    const [parsedVal, err] = this.decoder(this.currentValue);
+    if (err !== "") {
+      return err;
+    }
+    if (parsedVal === undefined) return;
+    const [preValue, _] = this.decoder(this.previousValue);
+    if (preValue === undefined) return;
+
+    for (const v of this.validators) {
+      const err = v(parsedVal as NoUndefined<T>, {
+        prevValue: preValue as NoUndefined<T>,
+        fieldName: this.fieldName,
+        all: this.form ? this.form.getValue() : {},
+      });
+      if (err != undefined) {
+        return err;
+      }
+    }
+  }
+
+  validate(): boolean {
+    const err = this._validate();
+    if (err == undefined) {
+      this.setError("");
+      return true;
+    }
+
     this.setError(err);
-    return !Boolean(err);
+    return false;
   }
 
   isValid(): boolean {
-    const [_, err] = this.validator(this.currentValue, {
-      prevValue: this.previousValue,
-      fieldName: this.fieldName,
-      all: this.parent.getData()
-    })
+    const err = this._validate();
     return !Boolean(err);
   }
 
@@ -84,36 +180,56 @@ export class Field<T, F> {
   }
 
   reset() {
-    this.setData(this.initialValue);
+    this.setValue(this.initialValue);
     this.makePristine();
   }
 
   setAndValidate(value: T) {
-    this.setData(value);
+    this.setValue(value);
     this.validate();
     return this.getError();
   }
 }
 
-type Fields<T> = {
-  [K in keyof T]: Field<T[K], T>
-}
-export class Form<T> {
-  getNotified: boolean = true
-  // @ts-ignore
-  fields: Fields<T>
+type Schema<T> = {
+  [K in keyof T]: Field<T[K]>;
+};
+type Values<T> = {
+  [K in keyof T]: T[K];
+};
 
-  constructor(public config: Config<T>) {}
+export const defaultConfig = {
+  multipleErrors: false,
+  stopOnError: false,
+};
+export class Form<T> {
+  getNotified: boolean = true;
+
+  constructor(
+    public fields: Schema<T>,
+    private config: Config<T> = defaultConfig
+  ) {
+    for (const fieldName in fields) {
+      fields[fieldName].form = this;
+      fields[fieldName].fieldName = fieldName;
+    }
+  }
+
+  initValue(values: Values<T>) {
+    for (const fieldName in this.fields) {
+      this.fields[fieldName].initValue(values[fieldName]);
+    }
+  }
 
   toggleGetNotified() {
     this.getNotified = !this.getNotified;
   }
 
-  setData(data: T, skipTrigger: boolean) {
+  setValue(data: T, skipTrigger: boolean) {
     this.toggleGetNotified();
     let prop: keyof typeof data;
     for (prop in data) {
-        this.fields[prop].setData(data[prop], skipTrigger);
+      this.fields[prop].setValue(data[prop], skipTrigger);
     }
     this.toggleGetNotified();
     if (skipTrigger) return;
@@ -122,7 +238,7 @@ export class Form<T> {
 
   triggerOnChange(): void {
     const callback = this.config.onChange;
-    this.getNotified && callback && callback(this.getData(), this);
+    this.getNotified && callback && callback(this.getValue(), this);
   }
 
   triggerOnError(): void {
@@ -130,27 +246,27 @@ export class Form<T> {
     this.getNotified && callback && callback(this.getError(), this);
   }
 
-  getData(): T {
-    const data = {} as T
-    let fieldName: keyof typeof this.fields
+  getValue(): T {
+    const data = {} as T;
+    let fieldName: keyof typeof this.fields;
     for (fieldName in this.fields) {
-        data[fieldName] = this.fields[fieldName].getData()
+      data[fieldName] = this.fields[fieldName].getValue();
     }
-    return data
+    return data;
   }
 
   getUpdates(): T {
-    const data = {} as T
-    let fieldName: keyof typeof this.fields
+    const data = {} as T;
+    let fieldName: keyof typeof this.fields;
     for (fieldName in this.fields) {
       if (this.fields[fieldName].isDirty()) {
-        data[fieldName] = this.fields[fieldName].getData()
+        data[fieldName] = this.fields[fieldName].getValue();
       }
     }
-    return data
+    return data;
   }
 
-  setError(errors: Error<T>, skipTrigger: boolean) {
+  setError(errors: Errors<T>, skipTrigger: boolean) {
     this.toggleGetNotified();
     let prop: keyof typeof errors;
     for (prop in errors) {
@@ -162,17 +278,17 @@ export class Form<T> {
     this.triggerOnError();
   }
 
-  getError(): Error<T> {
-    const errors = {} as Error<T>
-    let fieldName: keyof typeof this.fields
+  getError(): Errors<T> {
+    const errors = {} as Errors<T>;
+    let fieldName: keyof typeof this.fields;
     for (fieldName in this.fields) {
-        errors[fieldName] = this.fields[fieldName].getError()
+      errors[fieldName] = this.fields[fieldName].getError();
     }
-    return errors
+    return errors;
   }
 
   isDirty(): boolean {
-    let fieldName: keyof typeof this.fields
+    let fieldName: keyof typeof this.fields;
     for (fieldName in this.fields) {
       if (this.fields[fieldName].isDirty()) return true;
     }
@@ -181,7 +297,7 @@ export class Form<T> {
 
   makePristine() {
     this.toggleGetNotified();
-    let fieldName: keyof typeof this.fields
+    let fieldName: keyof typeof this.fields;
     for (fieldName in this.fields) {
       this.fields[fieldName].makePristine();
     }
@@ -191,7 +307,7 @@ export class Form<T> {
 
   reset() {
     this.toggleGetNotified();
-    let fieldName: keyof typeof this.fields
+    let fieldName: keyof typeof this.fields;
     for (fieldName in this.fields) {
       this.fields[fieldName].reset();
     }
@@ -205,7 +321,7 @@ export class Form<T> {
     this.toggleGetNotified();
 
     try {
-      let fieldName: keyof typeof this.fields
+      let fieldName: keyof typeof this.fields;
       for (fieldName in this.fields) {
         let validity: boolean;
         if (skipAttachError) {
@@ -241,45 +357,22 @@ export class Form<T> {
   }
 }
 
-type Error<T> = {
-  [K in keyof T]: string
-}
-
-export type Config<T> = {
-  onChange?: (data: T, form: Form<T>) => void
-  onError?: (error: Error<T>, form: Form<T>) => void
-  multipleErrors: boolean
-  stopOnError: boolean
+type Errors<T> = {
+  [K in keyof T]: string;
 };
 
-export type Context<T,F> = {
-  prevValue: T
-  fieldName: string
-  all: F
-}
-export type Validator<T, F> = (val: T, ctx?: Context<T, F>) => [T, string]
+export type Config<T> = {
+  onChange?: (data: T, form: Form<T>) => void;
+  onError?: (error: Errors<T>, form: Form<T>) => void;
+  multipleErrors: boolean;
+  stopOnError: boolean;
+};
 
-type Schema<T> = {
-  [K in keyof T]: Validator<T[K], T>
-}
+export type Context<T> = {
+  prevValue: T;
+  fieldName: string;
+  all: Record<string, any>;
+};
 
-export const defaultConfig  = {
-  multipleErrors: false,
-  stopOnError: false,
-}
-export function powerform<T extends Record<string, any>>(initialValues: T, schema: Schema<T>, config: Config<T> = defaultConfig): Form<T> {
-  const form = new Form<T>(config);
- 
-  // form.getNotified = true;
-
-  const fields = {} as Fields<T>
-  for (const fieldName in schema) {
-    const field = new Field(schema[fieldName], initialValues[fieldName], fieldName, form);
-    fields[fieldName] = field;
-  }
-
-  form.fields = fields
-  form.setData(initialValues, true);
-  return form
-}
-
+type NoUndefined<T> = T extends undefined ? never : T;
+export type Validator<T> = (val: T, ctx?: Context<T>) => string | undefined;
